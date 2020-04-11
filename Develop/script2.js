f83a24db11758cdeed2dcef9317012a2d7a90ac5
vjeuel <vjeuel@gmail.com>
2020-04-11 10:10:15 -0500

    Changed html to root

--- Develop/script2.js
+++ Develop/script2.js
@@ -8,41 +8,43 @@ let specChar = "!@#$%^&*_+-=";
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", trueConditions);
 
+
 // Function to determine how many characters
 // function Length() {
-//    let passLength = "";
-//    passLength = prompt("Choose between 8 and 128 characters");
-//    while (passLength < 8 || passLength > 128) {
-//       passLength = prompt("Please, choose a number between 8 and 128 characters!");
-//    };
-//    typeChar();
-// };
-
-// Function to determine how many characters
-let askNumber = "";
-let askLower = "";
-let askUpper = "";
-let askSpecChar = "";
-function typeChar() {
-   askNumber = confirm("Click OK if you would like NUMBERS and CANCEL if not.");
-   askLower = confirm("Click OK if you would like LOWERCASE characters and CANCEL if not.");
-   askUpper = confirm("Click OK if you would like UPPERCASE characters and CANCEL if not.");
-   askSpecChar = confirm("Click OK if you would like SPECIAL characters and CANCEL if not.");
-};
-
-
-
-// Maybe I need to use a IF IF here
-
-
-// If True Conditions add to Box, Else start over
-function trueConditions() {
-   passLength = prompt("Choose between 8 and 128 characters");
-   while (passLength < 8 || passLength > 128) {
-      passLength = prompt("Please, choose a number between 8 and 128 characters!");
-   };
-   typeChar();
-   
+   //    let passLength = "";
+   //    passLength = prompt("Choose between 8 and 128 characters");
+   //    while (passLength < 8 || passLength > 128) {
+      //       passLength = prompt("Please, choose a number between 8 and 128 characters!");
+      //    };
+      //    typeChar();
+      // };
+      
+      // Function to determine how many characters
+      let askNumber = "";
+      let askLower = "";
+      let askUpper = "";
+      let askSpecChar = "";
+      function typeChar() {
+         askNumber = confirm("Click OK if you would like NUMBERS and CANCEL if not.");
+         askLower = confirm("Click OK if you would like LOWERCASE characters and CANCEL if not.");
+         askUpper = confirm("Click OK if you would like UPPERCASE characters and CANCEL if not.");
+         askSpecChar = confirm("Click OK if you would like SPECIAL characters and CANCEL if not.");
+      };
+      
+      
+      
+      // Maybe I need to use a IF IF here
+      
+      
+      // If True Conditions add to Box, Else start over
+      function trueConditions() {
+         passLength = prompt("Choose between 8 and 128 characters");
+         while (passLength < 8 || passLength > 128) {
+            passLength = prompt("Please, choose a number between 8 and 128 characters!");
+         };
+         typeChar();
+         console.log(passLength);
+         
    if (askNumber || askLower || askUpper || askSpecChar) {
       document.querySelector("#password").value = "";
       let sumAllChar = "";
@@ -63,7 +65,7 @@ function trueConditions() {
          sumAllChar += uppercase.charAt(addUpper);
       };
       console.log(askUpper);
-
+      
       if (askSpecChar === true) {
          addSpecChar = Math.floor(Math.random() * specChar.length);
          sumAllChar += specChar.charAt(addSpecChar);
@@ -78,6 +80,15 @@ function trueConditions() {
    };
 };
 
+// Loop Password Generator
+function password(sumAllChar) {
+   let pwd = "";
+   for (let i = 0; i < chooseLen; i++) {
+      pwd += sumAllChar.charAt(Math.floor(Math.random() * sumAllChar.length));
+   }
+   return pwd;
+}
+console.log(sumAllChar);
 
 // Function to print the Password on screen
 function writePass(sumAllChar) {
@@ -91,15 +102,15 @@ function writePass(sumAllChar) {
    //    alert("For your PASSWORD to be generated you need to CHOOSE at least one type of character");
    // };
    
-
+   
    
    // No false Statement
-
-
-// // Function in case none of the characters are chosen it loops back and starts over 
-// function noneChosen() {
-//    let noneChosen = askNumber === false && askLower === false && askUpper === false && askSpcChar === false;
-//    if (noneChosen) {
+   
+   
+   // // Function in case none of the characters are chosen it loops back and starts over 
+   // function noneChosen() {
+      //    let noneChosen = askNumber === false && askLower === false && askUpper === false && askSpcChar === false;
+      //    if (noneChosen) {
 //       alert("You need to pick at least one type of character");
 //       passLength();
 //    };
