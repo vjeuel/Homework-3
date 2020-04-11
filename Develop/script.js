837078ea2598d2784375e9952086ac814cdd7f98
Jeuel <vjeuel@gmail.com>
2020-04-11 08:36:38 -0500

    Latest version

--- Develop/script.js
+++ Develop/script.js
@@ -5,39 +5,94 @@ let uppercase = "ABCDEFGHIJKLMONPQRSTUVXWYZ";
 let specChar = "!@#$%^&*_+-=";
 let allChar = number + lowercase + uppercase + specChar;
 
+
 // Empties variables to be filled with Data
-let choseLen = "";
+let chooseLen = "";
 let addNumber = "";
 let addLower = "";
 let addUpper = "";
-let addSpcChar = "";
+let addSpecChar = "";
+
+
 
 // Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", allQuestions);
 
-
-function writePassword() {
-  // Questions (I'm not sure that I have to console.log all the answers but it is nice to see them printing in the console)
-  choseLen = prompt("Choose between 8 and 128 characters");
-  while (choseLen < 8 || choseLen > 128) {
-    choseLen = prompt("Please, choose a number between 8 and 128 characters!")
+// Loop Password Generator
+function password(sumAllChar) {
+  let pwd = "";
+  for (let i = 0; i < chooseLen; i++) {
+    pwd += sumAllChar.charAt(Math.floor(Math.random() * sumAllChar.length));
   }
+  return pwd;
+}
+
+// Questions 
+function allQuestions() {
+  chooseLen = prompt("Choose between 8 and 128 characters");
+  while (chooseLen < 8 || chooseLen > 128) {
+    chooseLen = prompt("Please, choose a number between 8 and 128 characters!");
+  };
   
   addNumber = confirm("Click OK if you would like NUMBERS and CANCEL if not.");
   addLower = confirm("Click OK if you would like LOWERCASE characters and CANCEL if not.");
   addUpper = confirm("Click OK if you would like UPPERCASE characters and CANCEL if not.");
   addSpcChar = confirm("Click OK if you would like SPECIAL characters and CANCEL if not.");
   
+  // If False Conditions
   let allFalse = addNumber === false && addLower === false && addUpper === false && addSpcChar === false;
-  if (choseLen = allFalse) {
+  if (allFalse) {
     alert("You need to pick at least one type of character");
-    writePassword();
-  }
+    allQuestions();
+  }  
 };
-writePassword();
+allQuestions();
 
-console.log("Chosen length: " + choseLen)
+
+// Write password to the #password input
+function writePass(sumAllChar) {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+};
+  
+  // If True Conditions add to Box, Else start over
+  let sumAllChar = "";
+  if (addNumber === true) {
+    createNumber = Math.floor(Math.random() * number.length);
+    sumAllChar += number.charAt(createNumber);
+  }
+  
+  if (addLower === true) {
+    createLower = Math.floor(Math.random() * lowercase.length);
+    sumAllChar += lowercase.charAt(createLower);
+  }
+  if (addUpper === true) {
+    createUpper = Math.floor(Math.random() * uppercase.length);
+    sumAllChar += uppercase.charAt(createUpper);
+  }
+  if (addSpecChar === true) {
+    createSpecChar = Math.floor(Math.random() * specChar.length);
+    sumAllChar += specChar.charAt(createSpecChar);
+    
+    writePass(sumAllChar);
+    
+  } else {
+    alert("For your PASSWORD to be generated you need to CHOOSE at least one type of character");
+    return;
+  }
+  
+  console.log(Array.from(allChar));
+
+  
+  
+  
+  
+  // console.log(sumNumber);
+  
+  
+console.log("Chosen length: " + chooseLen)
 console.log("OK for NUMBERS: " + addNumber);
 console.log("OK for LOWERCASE: " + addLower);
 console.log("OK for UPPERCASE: " + addUpper);
@@ -45,31 +100,27 @@ console.log("OK for SPECIAL characters: " + addSpcChar);
 
 
 
-function password(length) {
-  let pwd = "";
-  for (let i = 0; i < length; i++) {
-    pwd += allChar.charAt(Math.floor(Math.random() * allChar.length));
-  }
-  return pwd;
-}
+// chooseLen.value
 
-console.log(password(10, length));
+console.log(password(chooseLen)); //Very important
 
-if (number === true) {
-  addUpper = Math.floor(uppercase.length * Math.random())
-}
 
-// Write password to the #password input
-function writePass() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+  // document.getElementById("password").innerHTML = pwd;
   
-  passwordText.value = password;
-}
+
+
+// NEEDS FIXING
+// Print password to textarea
+// When asked for no Numbers || no lowercase, password still prints all characters
+// Maybe add this trues to writePassword
+
+
+
+
 
 // console.log(password(10, length));
-console.log(writePassword);
-console.log(writePass);
+// console.log(writePassword);
 
 
 // console.log(allChar);
